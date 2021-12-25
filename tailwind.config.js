0e96303f75dbe70bcbead0b3b0265f83330100dc
ashirbad <ashirbadbehera29@gmail.com>
2021-12-25 20:51:48 +0530

    ✨ setup themes and nextjs stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 module.exports = {
+	darkMode: 'class',
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		typography: theme => ({}),
+		extend: {
+			colors: {
+				gray: {
+					0: '#fff',
+					100: '#eeeeee',
+					200: '#e0e0e0',
+					300: '#bbbbbb',
+					400: '#666666',
+					500: '#444444',
+					600: '#2a2a2a',
+					700: '#1f1f1f',
+					800: '#181818',
+					900: '#0f0f0f',
+				},
+			},
+		},
 	},
-	plugins: [],
+	variants: {},
+	plugins: [require('@tailwindcss/typography')],
 };
